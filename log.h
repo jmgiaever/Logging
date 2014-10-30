5710873b014cd1ae8ee98d2b88adde9a22f5056c
Joachim M. G <jgi004@post.uit.no>
2014-10-30 18:58:36 +0100

    Init. rel.

---
+++ log.h
@@ -0,0 +1,20 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+typedef struct logfile {
+	FILE 	*f;
+	char	*filename;
+	unsigned long long t1, t2;
+} logfile_t;
+
+logfile_t *logfile_create( char *logfile_name );
+unsigned long long log_entry_start( logfile_t *logfile );
+void log_entry_end( logfile_t *logfile, int idx, int entries );
+void logfile_avg( logfile_t *logfile );
+void logfile_destroy( logfile_t *logfile );
+
+#endif
